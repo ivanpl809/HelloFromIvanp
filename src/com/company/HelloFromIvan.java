fab118835b9122239a3adef7fd5fdb26f32e59b2
Ivanpl809 <ivanpl809@gmail.com>
2021-01-30 23:02:09 -0600

    Initial commit

---
+++ src/com/company/HelloFromIvan.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class HelloFromIvan {
+
+    public void sayHello() {
+        // The end line character happens automatically when we use
+        // the println() function.
+        System.out.println("Hello From Ivan");
+    }
+
+    public static void main(String[] args) {
+        // Create an instance of the class, and call the function.
+        HelloFromIvan hello = new HelloFromIvan();
+        hello.sayHello();
+    }
+}
